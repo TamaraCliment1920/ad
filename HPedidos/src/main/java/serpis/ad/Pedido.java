e7199aa443df60786e7f2779ae2800195a9c282a
TamaraCliment1920 <tamaracliment@gmail.com>
2020-01-10 09:38:09 +0100

    Actualizado

--- HPedidos/src/main/java/serpis/ad/Pedido.java
+++ HPedidos/src/main/java/serpis/ad/Pedido.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 
-
+@Entity
 public class Pedido {
 
 	private Long id;
