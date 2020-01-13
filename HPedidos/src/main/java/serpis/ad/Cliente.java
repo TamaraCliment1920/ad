d4a91a3cb12eb5c27552a4023e0258998c16b5f8
TamaraCliment1920 <tamaracliment@gmail.com>
2020-01-13 10:37:18 +0100

    modificado cliente

--- HPedidos/src/main/java/serpis/ad/Cliente.java
+++ HPedidos/src/main/java/serpis/ad/Cliente.java
@@ -16,12 +16,15 @@ public class Cliente {
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id=id;
 	}
+	
 	public String getnombre() {
 		return nombre;
 	}
+	
 	public void setnombre(String nombre) {
 		this.nombre=nombre;
 	}
